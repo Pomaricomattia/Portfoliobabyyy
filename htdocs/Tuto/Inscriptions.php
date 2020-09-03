3cc9f2f2cb62cce8fce7c1c50067f1104a946f7c
Pomaricomattia <web2020.mattia@gmail.com>
2020-09-03 16:04:25 +0200

    modification inscriptions.php

--- htdocs/Tuto/Inscriptions.php
+++ htdocs/Tuto/Inscriptions.php
@@ -1,44 +1,41 @@
 <?php
 
-$bdd = new PDO('mysql:host=localhost;dbname=espace_membre','root','');
+$bdd = new PDO('mysql:host=localhost;dbname=espace_membre;port=3308','root','');
+$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 if(isset($_POST['forminscription'])){
-    
-        $pseudo = htmlspecialchars($_POST['pseudo']);
-        $mail = htmlspecialchars($_POST['mail']);
+    echo "coucou";
+        $pseudo = htmlspecialchars($_POST['Pseudo']);
+        $mail = htmlspecialchars($_POST['Mail']);
         $mdp = sha1($_POST['mdp']);
         $mdp2= sha1($_POST['mdp2']);
 
-    if(!empty($_POST['pseudo']) AND !empty($_POST['mail']) AND!empty($_POST['mdp']) AND!empty($_POST['mdp2']))
+    if(!empty($_POST['Pseudo']) AND !empty($_POST['Mail']) AND!empty($_POST['mdp']) AND!empty($_POST['mdp2']))
     {
         
 
         $pseudolength = strlen($pseudo);
-        if($pseudolenght <= 255)
+        if($pseudolength <= 255)
         {
-          if($mail == $mail){
-              if(filter_var($mail,FILTER_VALIDATE_EMAIL))
-              {
-            $reqmail = $bdd->prepare("SELECT * FROM membres WHERE mail = ?");
-            $reqmail->execute(array($mail));
-            $mailexist = $reqmail-> rowCount();
-            if($mailexist == 0)
-            {
-              
-                if($mdp == $mdp2) {
-                    $insertmbr = $bdd->prepare("INSERT INTO membres(pseudo, mail, motdepasse) VALUES (? , ?, ?)");
-                    $insertmbr ->execute(array($pseudo,$mail,$mdp));
-                    $_SESSION['comptecree'] = "votre compte a bien été créé";
-                    header('Location: ../index.php');
-                }
-                else {
-                $erreur = " Vos mot de passe ne correspondent pas !";
-                }   
-            }
-            else {
-                $erreur = "Adresse mail deja utilisée";
-            }
-                }
+
+              if(filter_var($mail,FILTER_VALIDATE_EMAIL)) {
+                  $reqmail = $bdd->prepare("SELECT * FROM membres WHERE Mail = ?");
+                  $reqmail->execute(array($mail));
+                  $mailexist = $reqmail->rowCount();
+                  if ($mailexist == 0) {
+
+                      if ($mdp == $mdp2) {
+                          $insertmbr = $bdd->prepare("INSERT INTO membres(Pseudo, Mail, mdp) VALUES (? , ?, ?)");
+                          $insertmbr->execute(array($pseudo, $mail, $mdp));
+                          $_SESSION['comptecree'] = "votre compte a bien été créé";
+                          header('Location: ../index.php');
+                      } else {
+                          $erreur = " Vos mot de passe ne correspondent pas !";
+                      }
+                  } else {
+                      $erreur = "Adresse mail deja utilisée";
+                  }
+              }
                 
          else {
              $erreur = "Votre adresse mail n'est pas valide !";
@@ -53,14 +50,14 @@ if(isset($_POST['forminscription'])){
         $erreur ="tous les champs doivent être remplis !";
     }
 }
-}
+
 
 
 
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
@@ -68,26 +65,26 @@ if(isset($_POST['forminscription'])){
     <title>Document</title>
 </head>
 <body>
-    <div align="center" > 
+    <div align="center" >
     <h2>Inscriptions</h2>
-    <div  class="cadre" > 
-
-    <form method="POST" action=""> 
+    <div  class="cadre" >
+TEST
+    <form method="POST"  action="">
     <table>
     <tr>
     <td>
     <label for="pseudo">Pseudo :</label>
     </td>
     <td>
-    <input type="text" placeholder="Votre pseudo" name="pseudo" />
+    <input type="text" placeholder="Votre pseudo" name="Pseudo" />
     <td>
-    </tr> 
+    </tr>
     <tr>
     <td>
     <label for="email">Mail : </label>
     </td>
     <td>
-    <input type="email" placeholder="Votre mail" id="mail" name="mail"  />
+    <input type="email" placeholder="Votre mail" id="mail" name="Mail"  />
     <td>
     </tr>
     <tr>
@@ -109,12 +106,13 @@ if(isset($_POST['forminscription'])){
     <tr>
     <td></td>
     <td>
-    <input type="submit" value="Inscription"/>
+        <input type="hidden" name="forminscription" value="Inscription"/>
+    <input type="submit"   value="Inscription"/>
     </tr>
     </td>
     </table>
     </form>
-    </div>  
+    </div>
     <?php
     var_dump($_POST);
     if(isset($erreur))
